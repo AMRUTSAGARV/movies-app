e9395bf018ca6cb94349454b534580b16c267296
Amrut Sagar <amrutsagar06@gmail.com>
2022-02-08 22:59:57 +0530

    movies app

---
+++ movies-app/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   
+   <span>🎥 ENTERTAINMENT HUB 🎥  </span>
+    </div>
+  );
+}
+
+export default App;
