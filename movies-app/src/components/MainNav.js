8346990e37ca94b09d09937f5b3a066e0c989108
Amrut Sagar <amrutsagar06@gmail.com>
2022-02-09 16:11:57 +0530

    mainnav

--- movies-app/src/components/MainNav.js
+++ movies-app/src/components/MainNav.js
@@ -1,71 +1,33 @@
-import React, { useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import BottomNavigation from "@material-ui/core/BottomNavigation";
-import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
-import TvIcon from "@material-ui/icons/Tv";
-import MovieIcon from "@material-ui/icons/Movie";
-import SearchIcon from "@material-ui/icons/Search";
-import WhatshotIcon from "@material-ui/icons/Whatshot";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
-
-//useStyles used for material UI's inline styling or infile styling
 const useStyles = makeStyles({
-    root: {
-        width: "100%",
-        position: "fixed",
-        bottom: 0,
-        backgroundColor: "#2d313a",
-        zIndex: 100,
-    },
+  root: {
+    width: 500,
+  },
 });
 
 export default function SimpleBottomNavigation() {
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-    const navigate = useNavigate();
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-    useEffect(() => {
-        if (value === 0) {
-            navigate("/");
-        } else if (value === 1) {
-            navigate("/movies");
-        } else if (value === 2) {
-            navigate("/series");
-        } else if (value === 3) {
-            navigate("/search");
-        }
-    }, [value, navigate]);
-
-    return (
-        <BottomNavigation
-            value={value}
-            onChange={(event, newValue) => {
-                setValue(newValue);
-            }}
-            showLabels
-            className={classes.root}
-        >
-            <BottomNavigationAction
-                style={{ color: "white" }}
-                label="Trending"
-                icon={<WhatshotIcon />}
-            />
-            <BottomNavigationAction
-                style={{ color: "white" }}
-                label="Movies"
-                icon={<MovieIcon />}
-            />
-            <BottomNavigationAction
-                style={{ color: "white" }}
-                label="TV Series"
-                icon={<TvIcon />}
-            />
-            <BottomNavigationAction
-                style={{ color: "white" }}
-                label="Search"
-                icon={<SearchIcon />}
-            />
-        </BottomNavigation>
-    );
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+    </BottomNavigation>
+  );
 }
