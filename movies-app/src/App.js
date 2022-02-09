16529c6cc37e6b5a0bc24cd05cc38b35143addb4
Amrut Sagar <amrutsagar06@gmail.com>
2022-02-09 21:19:01 +0530

    pages created

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,15 +1,37 @@
 // import logo from './logo.svg';
+import { Container } from '@material-ui/core';
+// import Search from '@material-ui/icons/Search';
+// import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import Header from "./components/Header/Header";
 import SimpleBottomNavigation from './components/MainNav';
+import Trending from './Pages/Trending/Trending';
+import Movies from './Pages/Movies/Movies';
+import Search from './Pages/Search/Search';
+import Series from './Pages/Series/Series';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>     
       <Header />
-    <div className="app">Hello </div>
+    <div className="app">
+    <Container>
+
+      <Switch>
+<Route path='/' component={Trending} exact/>
+<Route path='/movies' component={Movies}/>
+<Route path='/series' component={Series}/>
+<Route path='/search' component={Search}/>
+
+      </Switch>
+    </Container>
+       </div>
+
+
+
     <SimpleBottomNavigation />
-    </>
+    </BrowserRouter>
   );
 }
 
--- movies-app/src/components/MainNav.js
+++ movies-app/src/components/MainNav.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
 
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import MovieIcon from '@material-ui/icons/Movie';
+import SearchIcon from '@material-ui/icons/Search';
+import TvIcon from '@material-ui/icons/Tv';
+import { useHistory } from 'react-router-dom';
+
+
+//this is usestyles used for material ui inline styling 
 const useStyles = makeStyles({
   root: {
-    width: 500,
+    width: "100%",
+    position:"fixed",
+    bottom:0,
+    backgroundColor: "#2d313a",
+    zindex:100,
   },
 });
 
 export default function SimpleBottomNavigation() {
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (value == 0) history.push("/");
+    else if(value ===1) history.push("/movies");
+    else if(value ===2) history.push("/series");
+    else if(value ===3) history.push("/search");
+    
+  }, [value, history]);
 
   return (
     <BottomNavigation
@@ -25,9 +43,29 @@ export default function SimpleBottomNavigation() {
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction 
+      style={{ color: "white "}} 
+      label="Trending" 
+      icon={<WhatshotIcon />} 
+      />
+
+      <BottomNavigationAction  
+      style={{ color: "white "}}  
+      label="Movies" 
+      icon={<MovieIcon />} 
+      />
+
+      <BottomNavigationAction  
+      style={{ color: "white "}}  
+      label="TV Series" 
+      icon={<TvIcon />} 
+      />
+      <BottomNavigationAction  
+      style={{ color: "white "}}  
+      label="Search" 
+      icon={<SearchIcon />} 
+      />
+
     </BottomNavigation>
   );
 }
